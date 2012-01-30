7ec7ccc57473f15b73dfe1c5892dd3924d357cf9
abirvalg <aribvalg@leopardflower>
2012-01-30 00:00:00 +0000

    bump up version number

--- version.h
+++ version.h
@@ -1,2 +1,2 @@
 /*This file is an placeholder for the version string. When source tree is getting ready for packaging, this file will be rewritten with #define VERSION "1.2.3" type of  string */
-#define VERSION "debug release" 
+#define VERSION "0.4"
